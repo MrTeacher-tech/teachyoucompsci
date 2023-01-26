5dd6ae8fcad7fc923eccf88800493a34bacd9472
MrTeacher-tech <81711321+MrTeacher-tech@users.noreply.github.com>
2023-01-26 12:57:37 -0800

    Add files via upload

---
+++ Triangles On Coordinate Plane.py
@@ -0,0 +1,44 @@
+import turtle
+
+t = turtle.Turtle()
+
+'''
+Use the goto( ) function to draw an Isosceles triangle
+'''
+
+t.goto(0,0)
+t.goto(40,100)
+t.goto(80,0)
+t.goto(0,0)
+
+
+'''
+Use the goto( ) function to draw an Equilateral triangle inside of your isosceles triangle
+'''
+
+t.goto(0,0)
+t.goto(40,60)
+#your code below here
+t.goto(80,0)
+t.goto(0,0)
+
+
+'''
+Use the goto( ) function to draw the same Equilateral triangle reflected across the x-axis
+'''
+
+t.goto(0,0)
+t.goto(40,-60)
+#your code below here
+t.goto(80,0)
+t.goto(0,0)
+
+
+'''
+Use the goto( ) function to draw the same Isosceles triangle reflected over the x-axis
+'''
+
+t.goto(0,0)
+t.goto(40,-100)
+t.goto(80,0)
+t.goto(0,0)
