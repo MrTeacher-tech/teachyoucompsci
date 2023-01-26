c3d1cc23fc1350e293cc449270c2e9b5f0cd777b
MrTeacher-tech <81711321+MrTeacher-tech@users.noreply.github.com>
2023-01-26 12:46:49 -0800

    Add files via upload

---
+++ Min Max Function.py
@@ -0,0 +1,21 @@
+def min_max(data):
+
+  min = data[0]
+  
+  max = data[0]
+
+  for i in data:
+    
+    #your code here
+   
+  return min, max
+
+#below, we run the min_max function to see if it works
+data = [10, 22, 8, 16, 45, 13]
+
+min, max = min_max(data)
+
+print("min value: ", min)
+print("max value: ", max)
+
+
